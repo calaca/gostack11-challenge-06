99c77be9e0a1fe438d0891f037039f7eb39c425d
Lorena Calaça <lorena.calaca.od@gmail.com>
2020-04-25 17:44:48 -0300

    Create and show transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,65 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  try {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    /**
+     * TODO:
+     * 1. Show category object instead of category_id
+     */
+    const transactions = await transactionsRepository.find();
+
+    const balance = await transactionsRepository.getBalance();
+
+    return response.status(200).json({ transactions, balance });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  try {
+    const { title, value, type, category } = request.body;
+
+    const createTransaction = new CreateTransactionService();
+
+    const transaction = await createTransaction.execute({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    return response.status(200).json(transaction);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  try {
+    return response.send();
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 transactionsRouter.post('/import', async (request, response) => {
-  // TODO
+  try {
+    return response.send();
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default transactionsRouter;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,65 @@
+import { getCustomRepository, getRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+    let category_id;
+
+    // check balance if transaction type is outcome
+    if (type === 'outcome') {
+      const balance = await transactionsRepository.getBalance();
+
+      if (value > balance.total) {
+        throw Error('Cannot make outcomes greater than your balance');
+      }
+    }
+
+    // check if category exists
+    const categoryExists = await categoriesRepository.findOne({
+      where: `"title" ILIKE '${title}%'`,
+    });
+
+    if (categoryExists) {
+      category_id = categoryExists.id;
+    } else {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      const savedCategory = await categoriesRepository.save(newCategory);
+
+      category_id = savedCategory.id;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
